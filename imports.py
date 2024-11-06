9c83dac3f76dcabf7b8d3781c4d02db987c8341e
Pascaru Alex <pascaru10djalex@gmail.com>
2024-11-06 14:15:59 +0200

    Imports and main

---
+++ imports.py
@@ -0,0 +1,17 @@
+#This file have all the imports needed for the project
+
+#You can also run this in the terminal:
+#pip install librosa numpy matplotlib seaborn
+
+# Audio processing
+import librosa
+import librosa.display
+
+# Numerical operations
+import numpy as np
+
+# Plotting
+import matplotlib.pyplot as plt
+
+# Optional: Advanced visualization tools (if you want to use interactive plots or seaborn for styling)
+import seaborn as sns  # optional, for more aesthetic plots
---
+++ main.py
@@ -0,0 +1,12 @@
+import librosa, librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+signal, sr = librosa.load(librosa.util.example('brahms'))
+
+plt.figure(figsize=(20, 5))
+librosa.display.waveshow(signal, sr=sr)
+plt.title('Waveplot', fontdict=dict(size=18))
+plt.xlabel('Time', fontdict=dict(size=15))
+plt.ylabel('Amplitude', fontdict=dict(size=15))
+plt.show()
