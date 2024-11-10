7e4ee2da947c15dad1b7e7564c2029e6036c17a5
Pascaru Alex <pascaru10djalex@gmail.com>
2024-11-11 01:09:27 +0200

    data statistics done

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def split_data():
     data = np.load("./pca.npy")
     data_labels = np.load("./labels.npy")
 
-    ratio = 99 # 80:20 ratio training:test data
+    ratio = 80
     dataset = np.zeros(data.shape[0], dtype=np.short)
 
     for i in range(1, 11):
@@ -25,7 +25,70 @@ def split_data():
 
     return train_data, train_labels, test_data, true_labels, dataset
 
+def calculate_metrics(metric_true_labels, metric_predictions, num_classes):
+    precisions = []
+    recalls = []
+    f_scores = []
+
+    for class_id in range(num_classes):
+        # True Positives
+        tp = np.sum((metric_predictions == class_id) & (metric_true_labels == class_id))
+
+        # False Positives
+        fp = np.sum((metric_predictions == class_id) & (metric_true_labels != class_id))
+
+        # False Negatives
+        fn = np.sum((metric_predictions != class_id) & (metric_true_labels == class_id))
+
+
+        precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+        precisions.append(precision)
+
+        recall = tp / (tp + fn) if (tp + fn) > 0 else 0
+        recalls.append(recall)
+
+        f_score = (2 * precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+        f_scores.append(f_score)
+
+    return precisions, recalls, f_scores
+
+def computing_statistics(data_true_labels, data_predictions):
+
+    data_genres = {"blues": 0, "classical": 1, "country": 2,
+                   "disco": 3, "hiphop": 4, "jazz": 5, "metal": 6,
+                   "pop": 7, "reggae": 8, "rock": 9}
+
+    new_true_labels = np.array([data_genres[genre] for genre in data_true_labels])
+    new_predictions = np.argmax(data_predictions, axis=0)
+
+    precisions, recalls, f_scores = calculate_metrics(new_true_labels, new_predictions, len(data_genres))
+    accuracy = np.sum(new_predictions == new_true_labels) / len(new_true_labels)
+
+    print(' ' * 15, end='')
+    for genre in data_genres:
+        print(f"{genre:^11}", end='')
+
+    print(f"\n{'Precisions:':<15}", end='')
+    for genre in data_genres:
+        index = data_genres[genre]
+        print(f"{precisions[index]:^11.2f}", end='')
+
+    print(f"\n{'Recalls:':<15}", end='')
+    for genre in data_genres:
+        index = data_genres[genre]
+        print(f"{recalls[index]:^11.2f}", end='')
+
+    print(f"\n{'F-Scores:':<15}", end='')
+    for genre in data_genres:
+        index = data_genres[genre]
+        print(f"{f_scores[index]:^11.2f}", end='')
+
+    print(f"\n{'Accuracy:':<15}{accuracy * 100:.2f}%")
+
+
 train_data, train_labels, test_data, true_labels, dataset = split_data()
 predictions = gnb(train_data, train_labels, test_data)
-# eva = evaluation(predictions, true_labels, name)
-# predictions = np.argmax(predictions, axis = 0)
+computing_statistics(true_labels,predictions)
+
+
+
