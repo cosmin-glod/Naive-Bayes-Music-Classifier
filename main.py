89b9c6186975480d4ebb59d0bf2d8fdee7c1aad5
cosminglod <glod.cosmin@gmail.com>
2024-11-10 19:28:19 +0200

    split data function

---
+++ main.py
@@ -0,0 +1,37 @@
+import numpy as np
+import random
+
+def split_data():
+    '''
+    divides the dataset into training and test set
+
+    :returns: training_set, test_set and respective labels
+    '''
+
+    # load data
+    data = np.load("./pca.npy")
+    data_labels = np.load("./labels.npy")
+
+    # random.seed(SEED)
+    ratio = 80 # 80:20 ratio training:test data
+    dataset = np.zeros(data.shape[0], dtype=np.short)
+
+    for i in range(1, 11):
+        min_ = (i - 1) * 100 - (i // 7)
+        max_ = min_ + 100 - (i % 6 == 0)
+        train_choice = random.sample(range(min_, max_), ratio)  # ratio number of numbers n where: _min <= n < _max
+        dataset[train_choice] = 1
+
+    # get training data
+    train_idx = np.where(dataset == 1)
+    train_data = data[train_idx]
+    train_labels = data_labels[train_idx]
+
+    # get test data
+    test_idx = np.where(dataset == 0)
+    test_data = data[test_idx]
+    true_labels = data_labels[test_idx]
+
+    return train_data, train_labels, test_data, true_labels, dataset
+
+split_data()
